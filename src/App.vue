9fd71edcfb190f936f0eaeb7976d0c703ce82fa1
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 18:09:43 +0200

    148 using checkboxes and saving data in Arrays

--- src/App.vue
+++ src/App.vue
@@ -51,13 +51,15 @@
                             <input
                                     type="checkbox"
                                     id="sendmail"
-                                    value="SendMail"> Send Mail
+                                    value="SendMail"
+                                    v-model="sendMail"> Send Mail
                         </label>
                         <label for="sendInfomail">
                             <input
                                     type="checkbox"
                                     id="sendInfomail"
-                                    value="SendInfoMail"> Send Infomail
+                                    value="SendInfoMail"
+                                    v-model="sendMail"> Send Infomail
                         </label>
                     </div>
 
@@ -112,7 +114,7 @@
                         <p style="white-space: pre">Message: {{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
-                            <li></li>
+                            <li v-for="(item, i) in sendMail" :key="i">{{ item }}</li>
                         </ul>
                         <p>Gender:</p>
                         <p>Priority:</p>
@@ -133,7 +135,8 @@ export default {
         password: "",
         age: 27
       },
-      message: 'A text message'
+      message: 'A text message',
+      sendMail: []
     };
   }
 };
