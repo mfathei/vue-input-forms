f034f9809a5ef7c5781f4d13ca8bc1ba70b405d5
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-13 18:14:16 +0200

    149 using radio buttons

--- src/App.vue
+++ src/App.vue
@@ -71,13 +71,15 @@
                         <input
                                 type="radio"
                                 id="male"
-                                value="Male"> Male
+                                value="Male"
+                                v-model="gender"> Male
                     </label>
                     <label for="female">
                         <input
                                 type="radio"
                                 id="female"
-                                value="Female"> Female
+                                value="Female"
+                                v-model="gender"> Female
                     </label>
                 </div>
             </div>
@@ -116,7 +118,7 @@
                         <ul>
                             <li v-for="(item, i) in sendMail" :key="i">{{ item }}</li>
                         </ul>
-                        <p>Gender:</p>
+                        <p>Gender: {{ gender }}</p>
                         <p>Priority:</p>
                         <p>Switched:</p>
                     </div>
@@ -136,7 +138,8 @@ export default {
         age: 27
       },
       message: 'A text message',
-      sendMail: []
+      sendMail: [],
+      gender: 'Male'
     };
   }
 };
